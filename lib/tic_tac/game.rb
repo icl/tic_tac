5c92bc2ff6bc627970da975224e3270e11a57ec0
Ethan Soutar-Rau <ethan.soutar.rau@gmail.com>
2011-10-21 15:32:10 -0700

    move files for guard

--- lib/tic_tac/game.rb
+++ lib/tic_tac/game.rb
@@ -1,15 +1,32 @@
 module TicTac
   class Game
 
-  attr_accessor :dimension
-  def initialize options = {}
-    @dimension = options[:dimension] || 3
-    @state = options[:state] || Array.new.fill('-', 0, dimension_squared)
-    @conversions = {0 => 'O', 1 => 'X', 2 => false}
-  end
-  def dimension_squared
-    @dimension * @dimension
+    attr_accessor :dimension
+    def initialize options = {}
+      @dimension = options[:dimension] || 3
+      @state = options[:state] || Array.new.fill('-', 0, dimension_squared)
+      @conversions = {0 => 'O', 1 => 'X', 2 => false}
+    end
+
+    def dimension_squared
+      @dimension * @dimension
+    end
+
+    def over?
+      false
+    end
+
+    def winner
+      nil
+    end
+
+    def tied?
+      false
+    end
+
+    def random_move
+      Random.new.rand(1..dimension_squared)
+    end
   end
 
-  end
 end
--- spec/tic_tac/game_spec.rb
+++ spec/tic_tac/game_spec.rb
@@ -47,12 +47,23 @@ describe Game do
       game.should be_over
     end
 
+  end
+
+  describe "recognizing a tie" do
     it "should recognize a tie" do
-      game = Game.new(state: @tie_game)
+      game = Game.new(state: @tie_game).should be_tied
+    end
+
+    it "should not think a new game is a tie" do
+      Game.new.should_not be_tied
     end
   end
 
   describe "Finding a winner" do
+    it "should not think a new game has a winner" do
+      Game.new.winner.should be_nil
+    end
+
     it "should recognize if play X has won" do
       Game.new( state: @x_winner).winner.should == 'X'   
     end
@@ -60,51 +71,58 @@ describe Game do
     it "should recognize if player O has won" do
       Game.new( state: @o_winner).winner.should == 'O'
     end
+
+    it "should recognize a horizontal win"
+
+    it "should recognize a vertical win"
   end
 
   describe "Making good moves" do
     describe "next move" do
-    it "should respond with index of the next the next move" do
-      Game.new().next_move
-    end
-
-    it "should not move if the game is over" do
-      Game.new(state:@x_winner).next_move.should be_nil
-    end
-
-    it "should update its state if make_move! is called" do
-      game = Game.new()
-      game.make_move!
-    end
-
-    it "should persist in making moves even if a tie is a forgon conclusion" do
-      game = Game.new(state: ["X", "X", "O", "X",
-                             "-", "X", "O", "-", 
-                             "O", "X", "X", "-", 
-                             "X", "O", "O", "O"] , dimension: 4)
-
-      game.next_move.should_not be_nil
-
-    end
-    
-    describe "Making random moves" do
-  
-      it "should attempt to make a random move" do
-
-        Game.new(state: @new_board).random_move.should_not be_nil
-      end
-  
-      it "should not move a space that has already been used"   
-
-      it "should not move on a full board" do
-       game = Game.new(state: @cat_board)
-       
-       game.random_move.should be_nil
-
+      it "should respond with index of the next the next move" do
+        Game.new().next_move
       end
 
+      it "should not move if the game is over" do
+        Game.new(state:@x_winner).next_move.should be_nil
+      end
+
+      it "should update its state if make_move! is called" do
+        game = Game.new()
+        game.make_move!
+      end
+
+      it "should persist in making moves even if a tie is a forgon conclusion" do
+        game = Game.new(state: ["X", "X", "O", "X",
+                               "-", "X", "O", "-", 
+                               "O", "X", "X", "-", 
+                               "X", "O", "O", "O"] , dimension: 4)
+
+        game.next_move.should_not be_nil
+      end
     end
   end
+
+  describe "Making random moves" do
+
+    it "should attempt to make a random move" do
+      Game.new(state: @new_board).random_move.should_not be_nil
+    end
+
+    it "should move within the bounds of the game" do
+      100.times do 
+      (1..25).should include Game.new(dimension:5).random_move
+      end
+    end
+
+    it "should not move a space that has already been used" do
+
+    end
+
+    it "should not move on a full board" do
+     game = Game.new(state: @cat_board)
+     game.random_move.should be_nil
+    end
 
   end
 
